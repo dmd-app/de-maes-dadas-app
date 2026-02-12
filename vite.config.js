0f54c592494ea185a07f4778774e62752510f12d
root <root@srv1339978.hstgr.cloud>
2026-02-12 13:07:20 +0000

    Force cache bust to fix Vercel deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Force new hash generation
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].${Date.now()}.js`,
+        chunkFileNames: `assets/[name].${Date.now()}.js`,
+        assetFileNames: `assets/[name].${Date.now()}.[ext]`
+      }
+    }
+  }
 })
