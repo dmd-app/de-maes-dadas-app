62e9553561d10cf7c342d069ddf61d08913f06f3
root <root@srv1339978.hstgr.cloud>
2026-02-13 01:11:25 +0000

    Design Overhaul: Pop Pill Aesthetics (Nexa)

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,14 @@ export default {
   theme: {
     extend: {
       colors: {
-        'dmd-bg': '#F6F3EE',
-        'dmd-blue': '#2B4DBC',
-        'dmd-yellow': '#C78A1D',
-        'dmd-pink': '#FFBAF7',
-        'dmd-lime': '#DCFFA2',
-        'dmd-black': '#000000',
+        'pop-pink': '#F0C4D5',      // Fundo Principal
+        'pop-burgundy': '#7A1C38',  // Cor do Logo/Botão Forte
+        'pop-blue': '#0A3F6B',      // Azul Escuro
+        'pop-green': '#144D38',     // Verde Escuro
+        'pop-lime': '#C8E068',      // Verde Lima
+        'pop-orange': '#E67A48',    // Laranja
+        'pop-yellow': '#FDF0A6',    // Amarelo Claro
+        'pop-light': '#FDF3F6',     // Off-white rosado
       },
       fontFamily: {
         display: ['"Bricolage Grotesque"', 'sans-serif'],
